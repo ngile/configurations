4c21b1b6b123d756a2924c4302fea7642d739dd1
Georgi Kostadinov <gkostadi@cisco.com>
2017-09-04 02:04:58 +0300

    Add vim setup script

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p ~/.vim/{bundle,colors,ftplugin,indent,config}
+curl https://raw.githubusercontent.com/ngile/configurations/master/.vimrc -o ~/.vimrc
 curl https://raw.githubusercontent.com/ngile/configurations/master/.vim/config/functions.vim -o ~/.vim/config/functions.vim
 curl https://raw.githubusercontent.com/ngile/configurations/master/.vim/config/mapping.vim -o ~/.vim/config/mapping.vim
 curl https://raw.githubusercontent.com/ngile/configurations/master/.vim/config/settings.vim -o ~/.vim/config/settings.vim
@@ -8,6 +9,6 @@ curl https://raw.githubusercontent.com/vim-scripts/wombat256.vim/master/colors/w
 curl https://raw.githubusercontent.com/vim-scripts/groovyindent/master/indent/groovy.vim -o ~/.vim/indent/groovy.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-`vim +PluginInstall +qall`
+vim +PluginInstall +qall
 
 curl https://raw.githubusercontent.com/honza/vim-snippets/master/pythonx/vimsnippets.py -o ~/.vim/bundle/ultisnips/pythonx/vimsnippets.py
