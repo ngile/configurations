7a1802fe1336675aca7a93a2c0ccd316420805a8
Georgi Kostadinov <gkostadi@cisco.com>
2017-11-14 02:21:57 +0200

    Add qutebrowser

---
+++ qutebrowser/config.py
@@ -0,0 +1,18 @@
+c.url.start_pages = ['https://www.google.com']
+c.editor.command = ["vim", "-f", "{}"]
+c.hints.border = '3px solid #E3BE23'
+c.colors.prompts.border = '2px solid gray'
+c.content.plugins = True
+c.url.default_page = 'https://google.com'
+c.url.searchengines = {"DEFAULT": "https://google.com/?q={}"}
+c.fonts.completion.category = "bold 10pt Terminus"
+c.fonts.completion.entry = "10pt Terminus"
+c.fonts.debug_console = "10pt Terminus"
+c.fonts.downloads = "10pt Terminus"
+c.fonts.hints = "bold 10pt Terminus"
+c.fonts.keyhint = "12pt Terminus"
+c.fonts.messages.error = "8pt Terminus"
+c.fonts.messages.info = "10pt Terminus"
+c.fonts.messages.warning = "10pt Terminus"
+c.fonts.statusbar = "8pt bold Terminus"
+c.fonts.tabs = "8pt bold Terminus"
