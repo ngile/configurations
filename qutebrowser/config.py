e949c46fb974fabc3af0c7461845ce3b72957923
Georgi Kostadinov <georgi.kostadinov@softwaregroup-bg.com>
2017-11-15 19:41:02 +0200

    Update qutebrowser config file

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,10 +1,33 @@
+c.editor.command = ["gvim", "-f", "{}"]
+c.new_instance_open_target = "tab-bg"
+c.prompt.filebrowser = False
+c.completion.height = "30%"
+c.completion.web_history_max_items = 1000
+c.input.partial_timeout = 2000
+c.tabs.background = True
+c.tabs.favicons.show = False
+c.tabs.title.format = "{title}"
+c.downloads.location.directory = '/home/georgi/Downloads'
+c.content.cache.size = 52428800
+c.content.webgl = False
+c.content.host_blocking.enabled = False
+c.hints.border = "1px solid #CCCCCC"
+c.hints.mode = "number"
+c.hints.chars = "123456789"
+c.hints.min_chars = 1
+
+# searches
+c.url.searchengines['DEFAULT'] = 'https://google.com/search?hl=en&q={}'
+c.url.searchengines['y'] = 'https://www.youtube.com/results?search_query={}'
+c.url.searchengines['w'] = 'https://secure.wikimedia.org/wikipedia/en/w/index.php?title=Special%%3ASearch&search={}'
+c.url.searchengines['gh'] = 'https://github.com/search?q=org%3Asoftwaregroup-bg+{}'
+
+
 c.url.start_pages = ['https://www.google.com']
-c.editor.command = ["vim", "-f", "{}"]
-c.hints.border = '3px solid #E3BE23'
 c.colors.prompts.border = '2px solid gray'
 c.content.plugins = True
 c.url.default_page = 'https://google.com'
-c.url.searchengines = {"DEFAULT": "https://google.com/?q={}"}
+
 c.fonts.completion.category = "bold 10pt Terminus"
 c.fonts.completion.entry = "10pt Terminus"
 c.fonts.debug_console = "10pt Terminus"
